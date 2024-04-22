fbc660ccea919a7ef0d7c0a88b9ed3423f1894c6
Rart3x <ramjattankeny971@gmail.com>
2024-04-22 12:42:58 +0200

    Colleen/ASM - #Rework, now using systems call for open & read and display on 1

--- Colleen/ASM/Colleen.asm
+++ Colleen/ASM/Colleen.asm
@@ -1,26 +1,33 @@
 bits 64
 
 section .data
-    message db '#include <stdio.h>', 0x0A, 0x0A, \
-        'static char* print() {', 0x0A, \
-        '    return ("Hello, World!");', 0x0A, \
-        '}', 0x0A, 0x0A, \
-        'int main(void) {', 0x0A, \
-        '    //---------------------------------First Comment---------------------------------//', 0x0A, \
-        '    printf("%s\\n", print());', 0x0A, \
-        '    return (0);', 0x0A, \
-        '}', 0x0A, 0x0A, \
-        '//---------------------------------Second Comment---------------------------------//', 0x0A, \
+    filename    db 'Colleen.asm', 0
+    buffer      resb 4096
 
 section .text
     global _start
-    _start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, message
-        mov rdx, 317
-        syscall
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+_start:
+    mov     rdi, filename               ; RDI contains the file name to open
+    mov     rax, 2                      ; System call number for `open`
+    xor     rsi, rsi                    ; RSI contains flags and permissions (0 for just reading)
+    syscall
+
+    ; Call to the system call to read the content of the file
+    mov     rdi, rax                    ; RDI contains the file descriptor
+    mov     rsi, buffer                 ; RSI points to the buffer to store the read data
+    mov     rdx, 4096
+    mov     rax, 0                      ; System call number for `read`
+    syscall
+
+    ; Call to the system call to write the read content to standard output
+    mov     rdi, 1                      ; RDI contains the file descriptor for standard output (stdout)
+    mov     rsi, buffer                 ; RSI points to the buffer containing the read content
+    mov     rdx, rax                    ; RDX contains the number of bytes read (returned by the read call)
+    mov     rax, 1                      ; System call number for `write`
+    syscall
+
+    ; Terminate the program
+    mov     rax, 60
+    xor     rdi, rdi
+    syscall
